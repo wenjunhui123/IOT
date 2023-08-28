6daf00ed5c2b3a89855f2b1d7fce3e0b47ec1e59
wenjunhui <wenjunhui@cet-electric.com>
2023-08-28 10:24:38 +0800

    换肤功能优化

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import router from "./router";
 import i18n from "@/lang/index";
 
 // import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
+import "element-plus/theme-chalk/dark/css-vars.css";
 import "@/styles/index.scss";
 import "uno.css";
 const app = createApp(App);
